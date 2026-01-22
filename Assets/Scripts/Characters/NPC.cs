15dc1764786b3e65ddc8a12241fc65e8883659dc
alvaro.cobo <alvaro.cobo.7e6@itb.cat>
2026-01-22 16:37:18 +0100

    add NPC dialogue system and related assets

--- Assets/Scripts/Characters/NPC.cs
+++ Assets/Scripts/Characters/NPC.cs
@@ -1,18 +1,87 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
-
+[RequireComponent(typeof(Collider2D))]
 public class NPC : MonoBehaviour, IInteractable
 {
     [SerializeField] private NPCDialogue dialogueData;
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TMP_Text dialogueText, nameText;
     [SerializeField] private Image portraitImage;
+
+    private int _dialogueIndex;
+    private bool _isTyping, _isDialogueActive;
     
+    public bool CanInteract() => !_isDialogueActive;
+
     public void Interact()
     {
-        Debug.Log("NPC Interacted With");
+        if (_isDialogueActive) 
+            NextLine();
+        else 
+            StartDialogue();
+    }
+
+    private void StartDialogue()
+    {
+        _isDialogueActive = true;
+        _dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.npcName);
+        portraitImage.sprite = dialogueData.npcPortrait;
+
+        dialoguePanel.SetActive(true);
+        StartCoroutine(TypeLine());
+    }
+
+    private void NextLine()
+    {
+        if (_isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[_dialogueIndex]);
+            _isTyping = false;
+        }
+        else
+        {
+            _dialogueIndex++;
+            if (_dialogueIndex < dialogueData.dialogueLines.Length)
+                StartCoroutine(TypeLine());
+            else
+                EndDialogue();
+        }
+    }
+
+    private IEnumerator TypeLine()
+    {
+        _isTyping = true;
+        dialogueText.SetText("");
+
+        foreach(char letter in dialogueData.dialogueLines[_dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        _isTyping = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.parent.TryGetComponent(out PlayerInputController player))
+        {
+            if (_isDialogueActive)
+                EndDialogue();
+        }
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        _isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
     }
 }
