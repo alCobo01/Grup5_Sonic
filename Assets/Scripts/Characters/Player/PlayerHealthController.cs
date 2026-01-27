7b7b5af29c4688e36fda7fa15bf14acdfcfd1da8
alvaro.cobo <alvaro.cobo.7e6@itb.cat>
2026-01-27 18:04:05 +0100

    started implementing health powerup

--- Assets/Scripts/Characters/Player/PlayerHealthController.cs
+++ Assets/Scripts/Characters/Player/PlayerHealthController.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 [RequireComponent(typeof(HealthBehaviour))]
 public class PlayerHealthController : MonoBehaviour, IDamageable
 {
+    [Header("Shield values")]
+    [SerializeField] private int maxShield = 50;
+
+    private int _currentShield;
     private HealthBehaviour _health;
 
     private void Awake()
@@ -11,11 +15,27 @@ public class PlayerHealthController : MonoBehaviour, IDamageable
     }
 
     private void OnEnable() => _health.OnDeath += HandleDeath;
-    
     private void OnDisable() => _health.OnDeath -= HandleDeath;
+
+    // Modify shield methods
+    private void ModifyShield(int amount) => _currentShield += amount;
     
-    public void TakeDamage(int damage) => _health.ModifyHealth(-Mathf.Abs(damage));
-    
+    // Modify health methods
+    public void AddHealth(int boost) => _health.ModifyHealth(boost);
+
+    public void TakeDamage(int damage)
+    {
+        // To compare with shield, we have to have positive values
+        damage = Mathf.Abs(damage);
+        if (damage <= _currentShield) _currentShield -= damage;
+        else if (damage > _currentShield)
+        {
+            damage -= _currentShield;
+            _currentShield = 0;
+            _health.ModifyHealth(-damage);
+        }
+        else _health.ModifyHealth(-damage);
+    }
     public void InstantKill() => _health.Kill();
     
     private void HandleDeath()
