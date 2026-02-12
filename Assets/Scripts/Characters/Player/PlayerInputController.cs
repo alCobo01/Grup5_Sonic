aef160c23b2a2129ed68fa984a346be3185749f1
✨ <miquel.thewandering@gmail.com>
2026-02-12 18:16:41 +0100

    refactor: adapt the old player script to new input accions

--- Assets/Scripts/Characters/Player/PlayerInputController.cs
+++ Assets/Scripts/Characters/Player/PlayerInputController.cs
@@ -9,7 +9,13 @@ public class PlayerInputController : MonoBehaviour, IPlayerActions
     public event UnityAction OnInteractEvent = delegate { };
     public static event UnityAction OnPauseGameEvent;
     public event UnityAction<bool> OnAimEvent = delegate { };
-    
+    public event UnityAction<Vector2> OnMoveEvent = delegate { };
+    public event UnityAction<Vector2> OnLookEvent = delegate { };
+    public event UnityAction OnJumpEvent = delegate { };
+    public event UnityAction OnCrouchHeldEvent = delegate { };
+    public event UnityAction OnCrouchReleasedEvent = delegate { };
+
+    private bool _crouchHeld;
     private InputSystem_Actions _inputActions;
     
     private void Awake()
@@ -20,7 +26,12 @@ public class PlayerInputController : MonoBehaviour, IPlayerActions
     
     private void OnEnable() => _inputActions.Enable();
     private void OnDisable() => _inputActions.Disable();
-    
+
+    private void Update()
+    {
+        if (_crouchHeld) OnCrouchHeldEvent.Invoke();
+    }
+
     public void OnAttack(InputAction.CallbackContext context)
     {
         if (context.performed) OnAttackEvent.Invoke();
@@ -40,5 +51,24 @@ public class PlayerInputController : MonoBehaviour, IPlayerActions
     public void OnPauseGame(InputAction.CallbackContext context)
     {
         if (context.performed) OnPauseGameEvent?.Invoke();
+    }
+
+    public void OnMove(InputAction.CallbackContext context) => OnMoveEvent.Invoke(context.ReadValue<Vector2>());
+
+    public void OnLook(InputAction.CallbackContext context) => OnLookEvent.Invoke(context.ReadValue<Vector2>());
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed) OnJumpEvent.Invoke();
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        if (context.performed) { _crouchHeld = true; }
+        if (context.canceled)
+        {
+            _crouchHeld = false;
+            OnCrouchReleasedEvent.Invoke();
+        }
     }
 }
