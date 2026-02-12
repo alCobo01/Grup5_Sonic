5d6712893a660e22b89673a987916151b5af479d
alvaro.cobo <alvaro.cobo.7e6@itb.cat>
2026-02-12 19:27:01 +0100

    add branch

--- Assets/Scripts/Characters/Attack/MeleeAttack.cs
+++ Assets/Scripts/Characters/Attack/MeleeAttack.cs
@@ -1,8 +1,14 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 [RequireComponent(typeof(AnimationBehaviour))]
 public class MeleeAttack : MonoBehaviour, IAttack
 {
+    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRange;
+    [SerializeField] private int damageAmount;
+    
     private AnimationBehaviour _animationBehaviour;
 
     private void Awake()
@@ -10,5 +16,17 @@ public class MeleeAttack : MonoBehaviour, IAttack
         _animationBehaviour = GetComponent<AnimationBehaviour>();
     }
 
-    public void Attack() => _animationBehaviour.TriggerMeleeAttack();
+    public void Attack()
+    {
+        _animationBehaviour.TriggerMeleeAttack();
+        var hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayer);
+
+        foreach (var enemy in hitEnemies)
+        {
+            if (enemy.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(damageAmount);
+            }
+        }
+    }
 }
