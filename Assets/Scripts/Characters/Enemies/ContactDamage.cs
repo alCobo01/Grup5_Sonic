f57d8cb9d4e43b666e31b41d8ca3381e609095fb
alvaro.cobo <alvaro.cobo.7e6@itb.cat>
2026-02-24 17:22:47 +0100

    fix: correct spelling of MeleeAttack and update attack cooldowns for enemies

--- Assets/Scripts/Characters/Enemies/ContactDamage.cs
+++ Assets/Scripts/Characters/Enemies/ContactDamage.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 public class ContactDamage : MonoBehaviour
 {
     [SerializeField] private int damageAmount = 1;
+    [SerializeField] private float attackCooldown = 1.5f;
     [SerializeField] private LayerMask targetLayer;
 
     private AnimationBehaviour _animationBehaviour;
+    private float _elapsedTime;
 
     private void Awake() => _animationBehaviour = GetComponent<AnimationBehaviour>();
+    private void Update() => _elapsedTime += Time.deltaTime;
 
     private void OnTriggerEnter(Collider other) => TryDealDamage(other.gameObject);
     private void OnCollisionEnter(Collision other) => TryDealDamage(other.gameObject);
@@ -16,10 +19,12 @@ public class ContactDamage : MonoBehaviour
     private void TryDealDamage(GameObject target)
     {
         if (((1 << target.layer) & targetLayer) == 0) return;
+        if (_elapsedTime < attackCooldown) return;
         
         var damageable = target.GetComponentInParent<IDamageable>();
         damageable?.TakeDamage(damageAmount);
         
         _animationBehaviour.TriggerMeleeAttack();
+        _elapsedTime = 0;
     }
 }
